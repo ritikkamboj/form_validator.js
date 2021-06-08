eb631c8cca749986d317fab940cef45b83c0911a
ritik4314 <ritikkamboj4314@gmail.com>
2021-06-08 13:37:58 +0530

    instead of using if else , we uses array technique to do the same thing in js file

--- script.js
+++ script.js
@@ -30,59 +30,87 @@ function isValidEmail(email)
 
 }
 
+// checking required fields 
+function checkRequired(inputArr)
+{
+    inputArr.forEach(function(input) {
+
+            if(input.value.trim()==='')
+            {
+                // showError(input,`${input.id} is required` );  yaha pe function kyu banaya hoga , as {thi} line is aslo working 
+                showError(input,`${getFieldName(input)} is required` );
+            }
+        // console.log(input.value);
+        // console.log(input.id);
+        else {
+            showSuccess(input); 
+        }
+    }); 
+
+}
+//filename function defining 
+function getFieldName(input)
+{
+    return input.id.charAt(0).toUppercase() + input.id.slice(1);   // number given in slice argument , the new array is formed from that indexx position including elemnt at that index 
+
+}
+
 //event listeners
 
 form.addEventListener('submit',function(e){
 
     e.preventDefault();
+
+    checkRequired([username,email,password,password2]);
 // console.log('submit');    /*generally it is used to prevent submiy ,  when one click on submit button  */
 
-if(username.value === '')
-{
-    showError(username,"username is required");
+//  if(username.value === '')
+// {
+//     showError(username,"username is required");
     
 
-}
-else{ 
-    showSuccess(username);
+// }
+// else{ 
+//     showSuccess(username);
 
-}
-if(email.value === '')
-{
-    showError(email,"email is required");
+// }
+// if(email.value === '')
+// {
+//     showError(email,"email is required");
     
 
-} else if(!isValidEmail(email.value)) 
-{
-    showError(email,"email is not valid ")
+// } else if(!isValidEmail(email.value)) 
+// {
+//     showError(email,"email is not valid ")
 
-}
-else{ 
-    showSuccess(email);
+// }
+// else{ 
+//     showSuccess(email);
 
-}
+// }
 
-if(password.value === '')
-{
-    showError(password,"password is required");
+// if(password.value === '')
+// {
+//     showError(password,"password is required");
     
 
-}
-else{ 
-    showSuccess(password);
+// }
+// else{ 
+//     showSuccess(password);
 
-}
-if(password2.value === '')
-{
-    showError(password2,"password2 is required");
+// }
+// if(password2.value === '')
+// {
+//     showError(password2,"password2 is required");
     
 
-}
-else{ 
-    showSuccess(password2);
+// }
+// else{ 
+//     showSuccess(password2);
 
-}
+// }
 
+// above style is become messy if we uses morer types of input fileds , so we are using checkrequired 
 
 
 // console.log(username);
